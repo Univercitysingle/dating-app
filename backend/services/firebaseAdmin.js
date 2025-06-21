ed04dbeb3f489c40332725ac6f6afb7558fe753c
enadoctemp <119559756+enadoctemp@users.noreply.github.com>
2025-06-21 17:39:26 +1000

    Update firebaseAdmin.js

--- backend/services/firebaseAdmin.js
+++ backend/services/firebaseAdmin.js
@@ -1,8 +1,17 @@
-const admin = require("firebase-admin");
-const serviceAccount = require("../firebase-service.json");
+const admin = require('firebase-admin');
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+};
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
