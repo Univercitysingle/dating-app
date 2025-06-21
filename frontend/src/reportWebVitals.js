6833006f2d28257e9766976016be6c2dd2c13d1c
enadoctemp <119559756+enadoctemp@users.noreply.github.com>
2025-06-21 13:24:51 +1000

    Update reportWebVitals.js

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,3 +1,8 @@
+// reportWebVitals.js
+// This file enables measuring and reporting of web vitals in your React application.
+// Web Vitals include metrics like CLS (Cumulative Layout Shift), FID (First Input Delay), LCP (Largest Contentful Paint), etc.
+// More info: https://web.dev/vitals/
+
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
